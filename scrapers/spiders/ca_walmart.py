0d80461dca369568f7027b2a2c1439f9dd05d7b0
Steve Matos <stevematos1998>
2021-01-20 20:59:22 -0500

    Initial Commit

---
+++ scrapers/spiders/ca_walmart.py
@@ -0,0 +1,12 @@
+import scrapy
+
+from scrapers.items import ProductItem
+
+
+class CaWalmartSpider(scrapy.Spider):
+    name = "ca_walmart"
+    allowed_domains = ["walmart.ca"]
+    start_urls = ["https://www.walmart.ca/en/grocery/fruits-vegetables/fruits/N-3852"]
+
+    def parse(self, response):
+        pass
