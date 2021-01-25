bbefcf3cff58793381c4f8228d99155a7572271f
Steve Matos <stevematos1998>
2021-01-25 04:40:20 -0500

    [CLEAN] SCRAPER - deleting code comments

--- scrapers/spiders/ca_walmart.py
+++ scrapers/spiders/ca_walmart.py
@@ -10,10 +10,6 @@ class CaWalmartBot(scrapy.Spider):
     allowed_domains = ['walmart.ca']
     handle_httpstatus_list = [500, 503, 504, 400, 408, 403]
     start_urls = ['https://www.walmart.ca/en/grocery/fruits-vegetables/fruits/N-3852']
-    # start_urls = (
-    #     'https://httpbin.org/get',
-    #     'https://httpbin.org/redirect-to?url=http%3A%2F%2Fexample.com%2F',
-    # )
     header = {
         'Host': 'www.walmart.ca',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
@@ -22,25 +18,14 @@ class CaWalmartBot(scrapy.Spider):
         'Accept-Encoding': 'gzip, deflate, br',
         'Content-Type': 'application/text',
         'Connection': 'keep-alive',
-        # 'If-None-Match': '*'
     }
 
-    # def start_requests(self):
-    #     response = scrapy.FormRequest(self.start_urls[0],
-    #                        callback=self.parse,
-    #                                   headers=self.header)
-    #     print(response.headers)
-    #     return [response]
-
     def parse(self, response):
-        # url_test = ['/en/ip/pears-bartlett/6000187833002']
+
         for url in response.css('.product-link::attr(href)').getall():
             yield response.follow(url, callback=self.parse_follow,
                                   cb_kwargs={'url': url}, headers=self.header)
-        # for url in url_test:
-        #     yield response.follow(url, callback=self.parse_follow, cb_kwargs={'url': url},
-        #                           headers=self.header
-        #                           )
+
         next_page = response.css('#loadmore::attr(href)').get()
 
         if next_page is not None:
@@ -113,7 +98,5 @@ class CaWalmartBot(scrapy.Spider):
         item['branch'] = branch
         item['stock'] = stock
         item['price'] = price
-
-        print(item)
 
         yield item
