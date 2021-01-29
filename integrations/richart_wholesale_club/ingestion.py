f39b1a7030fbbad2782fe7dfbe825afc18348076
Steve Matos <stevematos1998>
2021-01-29 01:29:07 -0500

    [REFACTOR] INGESTION - Refactoring process ingestion

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -1,6 +1,7 @@
 import os
 import re
 import pandas as pd
+from pandas import DataFrame
 
 from sqlalchemy import create_engine
 from integrations.richart_wholesale_club.utils import lower_col_names, lower_columns, \
@@ -16,56 +17,59 @@ DB_DIR = os.path.join(PROJECT_DIR, 'db.sqlite')
 engine = create_engine(r'sqlite:///' + DB_DIR)
 
 
-# Process CSV files
-def process_csv_files():
-    print(DB_DIR)
-    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|")
-    prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|")
+def transform(products_df: DataFrame, prices_stock_df: DataFrame):
+    products_df_clean = products_df.copy()
+    prices_stock_df_clean = prices_stock_df.copy()
 
     # filter for STOCK > 0
-    prices_stock_df = prices_stock_df[prices_stock_df['STOCK'] > 0]
+    prices_stock_df_clean = prices_stock_df_clean[prices_stock_df_clean['STOCK'] > 0]
 
     # filter BRANCHES MM, RHSM
-    prices_stock_df = prices_stock_df.loc[prices_stock_df['BRANCH'].isin(['MM', 'RHSM'])]
+    prices_stock_df_clean = prices_stock_df_clean.loc[
+        prices_stock_df_clean['BRANCH'].isin(['MM', 'RHSM'])]
 
     # Delete null values
-    products_df.fillna(value='', inplace=True)
+    products_df_clean.fillna(value='', inplace=True)
 
     # Adding STORE and URL columns
-    products_df['STORE'] = 'Richarts'
-    products_df['URL'] = 'n/a'
+    products_df_clean['STORE'] = 'Richarts'
+    products_df_clean['URL'] = 'n/a'
 
     # Filtering SKU values of products_df present in prices_stock_df
-    filter = products_df['SKU'].isin(prices_stock_df['SKU'].values)
-    products_df = products_df[filter]
+    filter = products_df_clean['SKU'].isin(prices_stock_df_clean['SKU'].values)
+    products_df_clean = products_df_clean[filter]
 
     # Taking the lowest pricing and the total stock availability
-    branchproducts_df = prices_stock_df.groupby(['SKU', 'BRANCH'], as_index=False, sort=False,
-                                                group_keys=False).aggregate(
+    branch_products_df = prices_stock_df_clean.groupby(['SKU', 'BRANCH'], as_index=False,
+                                                       sort=False,
+                                                       group_keys=False).aggregate(
         {'PRICE': 'min', 'STOCK': 'sum'})
 
-    # Converting data types
-    prices_stock_df['BRANCH'] = prices_stock_df['BRANCH'].astype('category')
     # Cleaning & styling
-    products_df['DESCRIPTION'] = products_df['DESCRIPTION'].apply(lambda x: re.sub('<.*?>', '', x))
-    products_df['PACKAGE'] = products_df['DESCRIPTION'].apply(
+    products_df_clean['DESCRIPTION'] = products_df_clean['DESCRIPTION'].apply(
+        lambda x: re.sub('<.*?>', '', x))
+    products_df_clean['PACKAGE'] = products_df_clean['DESCRIPTION'].apply(
         lambda x: re.findall(r'(?!0)\d+\s?\w+\.?$', x))
-    products_df['PACKAGE'] = products_df['PACKAGE'].apply(''.join).apply(
+    products_df_clean['PACKAGE'] = products_df_clean['PACKAGE'].apply(''.join).apply(
         lambda x: x.replace('.', ''))
-    products_df['CATEGORY'] = products_df['CATEGORY'].str.cat(
-        [products_df['SUB_CATEGORY'], products_df['SUB_SUB_CATEGORY']], sep=' | ')
+    products_df_clean['CATEGORY'] = products_df_clean['CATEGORY'].str.cat(
+        [products_df_clean['SUB_CATEGORY'], products_df_clean['SUB_SUB_CATEGORY']], sep=' | ')
 
     # Drop columns not required
-    products_df = products_df.drop(
+    products_df_clean = products_df_clean.drop(
         columns=['SUB_CATEGORY', 'SUB_SUB_CATEGORY', 'ORGANIC_ITEM', 'KIRLAND_ITEM', 'BUY_UNIT',
                  'FINELINE_NUMBER',
                  'DESCRIPTION_STATUS'])
 
     # Using utils functions
-    capitalize_columns_names(products_df, ['NAME', 'BRAND', 'DESCRIPTION'])
-    lower_columns(products_df, ['CATEGORY', 'PACKAGE'])
-    lower_col_names(products_df, branchproducts_df)
+    capitalize_columns_names(products_df_clean, ['NAME', 'BRAND', 'DESCRIPTION'])
+    lower_columns(products_df_clean, ['CATEGORY', 'PACKAGE'])
+    lower_col_names(products_df_clean, branch_products_df)
 
+    return products_df_clean, branch_products_df
+
+
+def load(products_df: DataFrame, branch_products_df: DataFrame):
     # Loading to SQLite DB
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -78,7 +82,7 @@ def process_csv_files():
                          con=engine)
     sql_df['sku'] = sql_df['sku'].astype(int)
 
-    branches_df = pd.merge(sql_df, branchproducts_df, how='inner', on='sku')
+    branches_df = pd.merge(sql_df, branch_products_df, how='inner', on='sku')
     branches_df.rename(columns={'id': 'product_id'}, inplace=True)
     branches_df.drop(columns='sku', inplace=True)
 
@@ -86,6 +90,16 @@ def process_csv_files():
     session.commit()
 
     session.close()
+
+
+# Process CSV files
+def process_csv_files():
+    # Extract csv
+    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|")
+    prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|")
+
+    products_df, branch_products_df = transform(products_df, prices_stock_df)
+    load(products_df, branch_products_df)
 
 
 if __name__ == "__main__":
